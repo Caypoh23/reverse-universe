f861e21a76bfcd340995105b07269d39c941a37f
caypoh23 <caypoh23@gmail.com>
2021-07-08 22:29:45 +0500

    feat: movement and jump is improved. ledge climb function and animation added. dash ability added

---
+++ Assets/Scripts/PlayerAfterImageSprite.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAfterImageSprite : MonoBehaviour
+{
+    [SerializeField] private float activeTime = 0.1f;
+    private float _timeActivated;
+    private float _alpha;
+    [SerializeField] private float alphaSet = 0.8f;
+    private float _alphaMultiplier = 0.65f;
+
+    private Transform _player;
+
+    private SpriteRenderer _SR;
+    
+    private SpriteRenderer _playerSR;
+    [SerializeField] private Tag afterImageTag;
+
+    private Color _color;
+
+    private void OnEnable()
+    {
+        _SR = GetComponent<SpriteRenderer>();
+        _player = GameObject.FindGameObjectWithTag("Player").transform;
+        _playerSR = _player.GetComponent<SpriteRenderer>();
+        
+        _alpha = alphaSet;
+        _SR.sprite = _playerSR.sprite;
+        transform.position = _player.position;
+        transform.rotation = _player.rotation;
+        _timeActivated = Time.time;
+    }
+
+    private void Update()
+    {
+        _alpha *= _alphaMultiplier;
+        _color = new Color(1f, 1f, 1f, _alpha);
+        _SR.color = _color;
+        
+        if (Time.time >= (_timeActivated + activeTime))
+        {
+            /*
+            var playerTransform = _player.transform;
+            ObjectPooler.Instance.SpawnFromPool(afterImageTag, playerTransform.position, playerTransform.rotation);*/
+            gameObject.SetActive(false);
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,22 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     private float _movementInputDirection;
     private float _jumpTimer;
+    private float _turnTimer;
+    private float _wallJumpTimer;
+    private float _dashTimeLeft;
+    private float _lastImageXpos;
+    private float _lastDash = -100;
 
     private int _amountOfJumpsLeft;
     private int _facingDirection = 1;
+    private int _lastWallJumpDirection;
 
     private bool _isFacingRight = true;
     private bool _isWalking;
@@ -19,11 +26,26 @@ public class PlayerController : MonoBehaviour
     private bool _canNormalJump;
     private bool _canWallJump;
     private bool _isAttemptingToJump;
+    private bool _checkJumpMultiplier;
+    private bool _canMove;
+    private bool _canFlip;
+    private bool _hasWallJumped;
+    private bool _isTouchingLedge;
+    private bool _canClimbLedge = false;
+    private bool _ledgeDetected;
+    private bool _isDashing = false;
+
+    private Vector2 _ledgePosBot;
+    private Vector2 _ledgePos1;
+    private Vector2 _ledgePos2;
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Animator anim;
 
     [SerializeField] private int amountOfJumps = 1;
+    
+    
+    [SerializeField] private Tag afterImageTag;
 
     [SerializeField] private float movementSpeed = 10.0f;
     [SerializeField] private float jumpForce = 16.0f;
@@ -36,12 +58,24 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float wallHopForce;
     [SerializeField] private float wallJumpForce;
     [SerializeField] private float jumpTimerSet = 0.15f;
+    [SerializeField] private float turnTimerSet = 0.1f;
+    [SerializeField] private float wallJumpTimerSet = 0.5f;
+    [SerializeField] private float dashTime;
+    [SerializeField] private float dashSpeed;
+    [SerializeField] private float distanceBetweenImages;
+    [SerializeField] private float dashCooldown;
+
+    [SerializeField] private float ledgeClimbXOffset1 = 0f;
+    [SerializeField] private float ledgeClimbYOffset1 = 0f;
+    [SerializeField] private float ledgeClimbXOffset2 = 0f;
+    [SerializeField] private float ledgeClimbYOffset2 = 0f;
 
     [SerializeField] private Vector2 wallHopDirection;
     [SerializeField] private Vector2 wallJumpDirection;
 
     [SerializeField] private Transform groundCheck;
     [SerializeField] private Transform wallCheck;
+    [SerializeField] private Transform ledgeCheck;
 
     [SerializeField] private LayerMask whatIsGround;
 
@@ -49,6 +83,7 @@ public class PlayerController : MonoBehaviour
     private static readonly int IsGrounded = Animator.StringToHash("isGrounded");
     private static readonly int YVelocity = Animator.StringToHash("yVelocity");
     private static readonly int IsWallSliding = Animator.StringToHash("isWallSliding");
+    private static readonly int CanClimbLedge = Animator.StringToHash("canClimbLedge");
 
     private void Start()
     {
@@ -65,6 +100,8 @@ public class PlayerController : MonoBehaviour
         CheckIfCanJump();
         CheckIfWallSliding();
         CheckJump();
+        CheckLedgeClimb();
+        CheckDash();
     }
 
     private void FixedUpdate()
@@ -75,7 +112,7 @@ public class PlayerController : MonoBehaviour
 
     private void CheckIfWallSliding()
     {
-        if (_isTouchingWall && _movementInputDirection == _facingDirection)
+        if (_isTouchingWall && _movementInputDirection == _facingDirection && rb.velocity.y < 0 && !_canClimbLedge)
         {
             _isWallSliding = true;
         }
@@ -85,12 +122,65 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    private void CheckLedgeClimb()
+    {
+        if (_ledgeDetected && !_canClimbLedge)
+        {
+            _canClimbLedge = true;
+
+            if (_isFacingRight)
+            {
+                _ledgePos1 = new Vector2(Mathf.Floor(_ledgePosBot.x + wallCheckDistance) - ledgeClimbXOffset1,
+                    Mathf.Floor(_ledgePosBot.y) + ledgeClimbYOffset1);
+                _ledgePos2 = new Vector2(Mathf.Floor(_ledgePosBot.x + wallCheckDistance) + ledgeClimbXOffset2,
+                    Mathf.Floor(_ledgePosBot.y) + ledgeClimbYOffset2);
+            }
+            else
+            {
+                _ledgePos1 = new Vector2(Mathf.Ceil(_ledgePosBot.x - wallCheckDistance) +
+                                         ledgeClimbXOffset1, Mathf.Floor(_ledgePosBot.y) +
+                                                             ledgeClimbYOffset1);
+                _ledgePos2 = new Vector2(Mathf.Ceil(_ledgePosBot.x - wallCheckDistance) -
+                                         ledgeClimbXOffset2, Mathf.Floor(_ledgePosBot.y) +
+                                                             ledgeClimbYOffset2);
+            }
+
+            _canMove = false;
+            _canFlip = false;
+
+            anim.SetBool(CanClimbLedge, _canClimbLedge);
+        }
+
+        if (_canClimbLedge)
+        {
+            transform.position = _ledgePos1;
+        }
+    }
+
+    public void FinishLedgeClimb()
+    {
+        _canClimbLedge = false;
+        transform.position = _ledgePos2;
+        _canMove = true;
+        _canFlip = true;
+        _ledgeDetected = false;
+        anim.SetBool(CanClimbLedge, _canClimbLedge);
+    }
+
     private void CheckSurroundings()
     {
         _isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
 
         _isTouchingWall = Physics2D.Raycast(wallCheck.position, transform.right, wallCheckDistance, whatIsGround);
+        _isTouchingLedge = Physics2D.Raycast(ledgeCheck.position, transform.right, wallCheckDistance, whatIsGround);
+
+        if (_isTouchingWall && !_isTouchingLedge && !_ledgeDetected)
+        {
+            _ledgeDetected = true;
+            _ledgePosBot = wallCheck.position;
+        }
     }
+
 
     private void CheckIfCanJump()
     {
@@ -147,8 +237,74 @@ public class PlayerController : MonoBehaviour
             }
         }
 
-        if (Input.GetKeyUp(KeyCode.Space))
+        if (Input.GetButtonDown("Horizontal") && _isTouchingWall)
+        {
+            if (!_isGrounded && _movementInputDirection != _facingDirection)
+            {
+                _canMove = false;
+                _canFlip = false;
+
+                _turnTimer = turnTimerSet;
+            }
+        }
+
+        if (!_canMove)
+        {
+            _turnTimer -= Time.deltaTime;
+
+            if (_turnTimer <= 0)
+            {
+                _canMove = true;
+                _canFlip = true;
+            }
+        }
+
+        if (_checkJumpMultiplier && !Input.GetKey(KeyCode.Space))
+        {
+            _checkJumpMultiplier = false;
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * variableJumpHeightMultiplier);
+        }
+
+        if (Input.GetButtonDown($"Dash"))
+        {
+            AttemptToDash();
+        }
+    }
+
+    private void CheckDash()
+    {
+        if (_isDashing)
+        {
+            if (_dashTimeLeft > 0)
+            {
+                _canMove = false;
+                _canFlip = false;
+                rb.velocity = new Vector2(dashSpeed * _facingDirection, rb.velocity.y);
+                _dashTimeLeft -= Time.deltaTime;
+
+                if (Mathf.Abs(transform.position.x - _lastImageXpos) > distanceBetweenImages)
+                {
+                    ObjectPooler.Instance.SpawnFromPool(afterImageTag, transform.position, transform.rotation);
+                    _lastImageXpos = transform.position.x;
+                }
+            }
+
+            if (_dashTimeLeft <= 0 || _isTouchingWall)
+            {
+                _isDashing = false;
+                _canMove = true;
+                _canFlip = true;
+            }
+        }
+    }
+
+    private void AttemptToDash()
+    {
+        _isDashing = true;
+        _dashTimeLeft = dashTime;
+        _lastDash = Time.time;
+        ObjectPooler.Instance.SpawnFromPool(afterImageTag, transform.position, transform.rotation);
+        _lastImageXpos = transform.position.x;
     }
 
     private void CheckJump()
@@ -156,7 +312,8 @@ public class PlayerController : MonoBehaviour
         if (_jumpTimer > 0)
         {
             // WallJump
-            if (!_isGrounded && _isTouchingWall && _movementInputDirection != 0 && _movementInputDirection != _facingDirection)
+            if (!_isGrounded && _isTouchingWall && _movementInputDirection != 0 &&
+                _movementInputDirection != _facingDirection)
             {
                 WallJump();
             }
@@ -165,9 +322,27 @@ public class PlayerController : MonoBehaviour
                 NormalJump();
             }
         }
-        if(_isAttemptingToJump)
+
+        if (_isAttemptingToJump)
         {
             _jumpTimer -= Time.deltaTime;
+        }
+
+        if (_wallJumpTimer > 0)
+        {
+            if (_hasWallJumped && _movementInputDirection == _lastWallJumpDirection)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, 0.0f);
+                _hasWallJumped = false;
+            }
+            else if (_wallJumpTimer <= 0)
+            {
+                _hasWallJumped = false;
+            }
+            else
+            {
+                _wallJumpTimer -= Time.deltaTime;
+            }
         }
     }
 
@@ -179,6 +354,7 @@ public class PlayerController : MonoBehaviour
             _amountOfJumpsLeft--;
             _jumpTimer = 0;
             _isAttemptingToJump = false;
+            _checkJumpMultiplier = true;
         }
     }
 
@@ -203,6 +379,13 @@ public class PlayerController : MonoBehaviour
             rb.AddForce(forceToAdd, ForceMode2D.Impulse);
             _jumpTimer = 0;
             _isAttemptingToJump = false;
+            _checkJumpMultiplier = true;
+            _turnTimer = 0;
+            _canMove = true;
+            _canFlip = true;
+            _hasWallJumped = true;
+            _wallJumpTimer = wallJumpTimerSet;
+            _lastWallJumpDirection = -_facingDirection;
         }
     }
 
@@ -212,7 +395,7 @@ public class PlayerController : MonoBehaviour
         {
             rb.velocity = new Vector2(rb.velocity.x * airDragMultiplier, rb.velocity.y);
         }
-        else
+        else if (_canMove)
         {
             rb.velocity = new Vector2(movementSpeed * _movementInputDirection, rb.velocity.y);
         }
@@ -228,7 +411,7 @@ public class PlayerController : MonoBehaviour
 
     private void Flip()
     {
-        if (!_isWallSliding)
+        if (!_isWallSliding && _canFlip)
         {
             _facingDirection *= -1;
             _isFacingRight = !_isFacingRight;
